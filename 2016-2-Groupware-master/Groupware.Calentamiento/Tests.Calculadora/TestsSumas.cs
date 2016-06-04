3b5232203c56a9f34057d32eecd368bb12a5b3da
franciny1 <franciny.rosales1@gmail.com>
2016-06-04 16:14:29 -0600

    trabajo de clase 4

---
+++ 2016-2-Groupware-master/Groupware.Calentamiento/Tests.Calculadora/TestsSumas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core.Numero;
+
+namespace Tests.Calculadora
+{
+    [TestClass]
+    public class TestsSumas
+    {
+        [TestMethod]
+        public void UnoMasCuatro()
+
+        {
+
+            // definicion del ecenario
+            Numero elPrimerOperando = new Numero("1", 10);
+            Numero elSegundoOperando = new Numero("4", 10);
+            char elOperando = '+';
+            Numero elResultadoEsperado = new Numero("5", 10);
+            Numero elResultadoReal;
+
+            //invoque el metodo que se prueba
+            var laReferencia = new Core.Numero.Dominio.Acciones.Suma();
+            elResultadoReal = laReferencia.Operar(elPrimerOperando, elSegundoOperando);
+
+            //Verificar si el resultado obtenido es el mismo operando
+            Assert.AreEqual<Numero>(elResultadoEsperado, elResultadoReal);
+            
+
+        }
+    }
+}
